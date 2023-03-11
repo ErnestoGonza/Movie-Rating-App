8e6a9019e144adc8fd7e75d3ff2e4bc3afe7fb49
Ernesto Gonzalez <egonzalez442@gmail.com>
2023-03-11 15:23:08 -0500

    updated sign-in functionality to cross reference test with the encrypted password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,12 +5,12 @@ const userController = {};
 userController.createUser = async (req, res, next) => {
   const { name, email, password } = req.body;
 
-  const oldUser = await User.findOne({ email });
-
-  if (oldUser)
-    return res.status(409).json({ error: 'this email is already in use!' });
-
   try {
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser)
+      return res.status(409).json({ error: 'this email is already in use!' });
+
     const newUser = await User.create({ name, email, password });
     res.locals.newUser = newUser;
     return next();
@@ -27,13 +27,24 @@ userController.createUser = async (req, res, next) => {
 userController.signIn = async (req, res, next) => {
   const { email, password } = req.body;
 
-  const existingEmail = await User.findOne({ email });
+  try {
+    const existingEmail = await User.findOne({ email });
+    const comparePassword = await existingEmail.comparePassword(password);
 
-  if (!email || !existingEmail)
-    return res.status(401).json({ error: 'Invalid email!' });
-  if (!password) return res.status(401).json({ error: 'Invalid password!' });
+    if (!email || !existingEmail)
+      return res.status(401).json({ error: 'Invalid email!' });
+    if (!password || !comparePassword)
+      return res.status(401).json({ error: 'Invalid password!' });
 
-  return next();
+    return next();
+  } catch (err) {
+    return next({
+      status: 500,
+      message: err,
+      method: 'POST',
+      location: 'UserController.signIn',
+    });
+  }
 };
 
 module.exports = userController;
