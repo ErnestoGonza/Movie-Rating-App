82721d90f530a7214d8a748c6e80ba7d8b04f27c
Ernesto Gonzalez <egonzalez442@gmail.com>
2023-09-13 18:16:03 -0400

    Adds database and schema

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,16 @@
-const create = (req, res) => {
-  res.status(200).json({ user: req.body });
+const User = require('../models/userSchema');
+
+const create = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const oldUser = await User.findOne({ email });
+  if (oldUser)
+    return res.status(409).json({ error: 'Email is already in use!' });
+
+  const newUser = new User({ name, email, password });
+  await newUser.save();
+
+  res.status(201).json({ user: newUser });
 };
 
 module.exports = {
--- backend/db/index.js
+++ backend/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    'mongodb+srv://TheRaftMaker:cvIDRBvAk0XQKZBl@cluster0.dj98x50.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://TheRaftMaker:cvIDRBvAk0XQKZBl@cluster0.dj98x50.mongodb.net/review-app?retryWrites=true&w=majority'
   )
   .then(() => {
     console.log('db is connected!');
